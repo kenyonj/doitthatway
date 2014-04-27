d935518f96aa7ad809d4c397e6d77ea515b54c14
Justin Kenyon <kenyonj@gmail.com>
2014-04-27 15:10:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doitthatway::Application.config.secret_key_base = '9180fcd992368e0c22ad0bf90541d70225334050bc43655d97c81ff0f42f88926e0c34a1c060321ae791bb8afe38ced54d057534cb5a5c823e2105545f694601'
