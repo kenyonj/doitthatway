1122a660b2b40bbd0c4faf56e84cfff0ee2b785a
Justin Kenyon <kenyonj@gmail.com>
2014-05-14 18:37:04 -0400

    Redirect to last viewed video on logout or login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,17 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to random_video_path
+    redirect_to last_viewed_video
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to random_video_path
+    redirect_to last_viewed_video
+  end
+
+  private
+
+  def last_viewed_video
+    Video.find(last_viewed_video_id)
   end
 end
